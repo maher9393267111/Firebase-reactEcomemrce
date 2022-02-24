01d91064151dca0ed234455e937a7f0a80aea23d
Your Name <you@example.com>
2022-02-24 19:20:10 +0300

    ommman man

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const NotFound = () => {
     return (
         <div>
-            notfoun-------------------
+            notfoun-------------------123444
         </div>
     )
 }
